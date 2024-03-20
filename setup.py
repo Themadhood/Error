b4bba62da1fd5214306444de3fc2edb902c5285c
Themadhood's Codes <148127209+Themadhood@users.noreply.github.com>
2024-03-20 13:23:25 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+#from my_pip_package import __version__
+
+setup(
+    name='Error',
+    version="1.0.0",#__version__,
+
+    url='https://github.com/Themadhood/Error',
+    author='Themadhood Pequot',
+    author_email='themadhoodpequot@gmail.com',
+
+    py_modules=['__init__',"Log","Upload","TimeDefs"],
+)
